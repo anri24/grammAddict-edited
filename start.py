0b189bce6a489fe6b2e6845f3efdb4912097dda9
anri2407 <anrivarshanidze2407@gmail.com>
2023-06-08 16:04:57 +0400

    easy start

---
+++ start.py
@@ -0,0 +1,25 @@
+import os 
+import shutil
+import yaml
+import subprocess
+
+
+
+username = input('what is your accounts username? ')
+config_dir = 'accounts/'+username
+config_ex = 'config-examples'
+dest = 'accounts/'+username
+config = os.path.exists(config_dir)
+
+if(config == False):
+    noAccountFile = input("There is no account's config file, do you want to create new ? (yes/no) ") 
+    if (len(noAccountFile) > 0):
+        answer = noAccountFile[0]
+    if noAccountFile == '' or answer == "y":
+        shutil.copytree(config_ex, dest) 
+        print()
+        print('config file is created go inside and chage other importent things ('+ dest +'/config.yml)')
+    elif answer == "n":
+        exit 
+else:
+    os.system('python run.py --config accounts/'+ username +'/config.yml')
